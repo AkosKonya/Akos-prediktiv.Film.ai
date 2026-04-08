6b6a83b4e6107a35a0d86cbdc22460ef6fb3111f
AkosKonya <akos.konya@elev.ga.lbs.se>
2026-04-08 11:05:03 +0200

    Add files via upload

---
+++ Akos_prediktiv.Film.ai.py
@@ -0,0 +1,12 @@
+media =[
+    {"title": "Rush", "genre": "Action", "age": "13+", "runtime": "2:03"},
+    {"title": "Rush Hour", "genre": "Comdey", "age": "28+", "runtime": "1:38"},
+    {"title": "John Wick", "genre": "Action", "age": "12+", "runtime": "1:41"},
+    {"title": "Cars", "genre": "Adventure", "age": "20+", "runtime": "1:57"},
+    {"title": "Puss in Boots", "genre": "Adventure", "age": "15+", "runtime": "1:30"},
+    {"title": "Terminator", "genre": "Action", "age": "42+", "runtime": "1:47"},
+    {"title": "Terminator 2", "genre": "Action", "age": "34+", "runtime": "2:17"},
+    {"title": "Terminator 3", "genre": "Action", "age": "23+", "runtime": "1:49"},
+    {"title": "Cars 2", "genre": "Adventure", "age": "15+", "runtime": "1:46"},
+    {"title": "Matrix", "genre": "Action", "age": "27+", "runtime": "2:16"},
+   ]
